4275fcabd20d664a8ca698fb6465843804184045
You <hzyrz@icloud.com>
2018-04-01 21:23:57 -0400

    final

--- shotDetection.py
+++ shotDetection.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 
+THRESHOLD = 3
 def calculateHistogram(image):
     img = cv2.imread(image)
     color = ('b','g','r')
@@ -12,23 +13,27 @@ def calculateHistogram(image):
 
     return np.array(colorHistogram)
 
+def findCutScene(files):
+    cuts = []
+    for i in range(len(files)):
+        filename = files[i]
+        if (i == 0):
+            image = os.path.join('./clip', filename)
+            prev = calculateHistogram(image)
+            prevDiff = float('inf')
+        else:
+            cur = calculateHistogram(os.path.join('./clip', filename))
+            diff = np.linalg.norm(cur - prev)
+            if (diff - prevDiff) > THRESHOLD * prevDiff: cuts.append(i)
+            prev = cur
+            prevDiff = diff
+
+    return cuts
 
 directory = os.fsencode("./clip")
 files = os.listdir(directory)
 files = [os.fsdecode(file) for file in files]
 files = sorted(files, key=lambda x: (x.split('.')[0]))
-diff = []
-for i in range(len(files)):
-    filename = files[i]
-    if (i == 0):
-        image = os.path.join('./clip', filename)
-        prev = calculateHistogram(image)
-    else:
-        cur = calculateHistogram(os.path.join('./clip', filename))
-        diff.append(np.linalg.norm(cur - prev))
-        prev = cur
-
-print(np.argmax(diff))
-print(max(diff))
-print(diff)
+cuts = findCutScene(files)
+print(cuts)
 
